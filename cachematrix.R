8cf11b93486b4b74cbbef9542bc2cf6397857003
mustafafaisan <mustafa.faisan@gmail.com>
2015-08-21 11:55:11 +0530

    Update cachematrix.R
    Added performance test functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,6 @@
-## Put comments here that give an overall description of what your
+## Invesre of matrix is caclulated using a 2 methods
+## a. Noraml way b. Using Local cache
+## inverseMatrixNoCache and  inverseMatrixWithCache are used to assess its performance 
 ## functions do
 
 ## function to 
@@ -18,7 +20,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## cacheSolve returns inverse of the matrix from the cache
-## if the matrix is modified, inverse is recalculated and returned
+## if the matrix is modified, inverse is recalculated and returned 
+
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -28,14 +31,77 @@ cacheSolve <- function(x, ...) {
         return(inv)
     }
     matData = x$get()
-    inv = solve(matData)
-    x$setinverse(inv)
+    tryCatch({
+        inv = solve(matData)
+        x$setinverse(inv)
+    }, error = function(err){
+        print("Error occured while solving for inverse of matrix")
+    }, finally = {
+        inv
+    })
     inv
 }
 
-# To Genreate a matrix whose inverse exists
+# To Genreate a matrix for inverse calculation
 hilbert <- function(n) { 
     i <- 1:n; 1 / outer(i - 1, i, "+") 
+}
+
+# solve inverse without cache
+inverseMatrixNoCache <- function(n, size){
+    # n - no of iterations
+    # size is the size of the matrix, it will be square matrix
+    
+    # This will try to find time taken for n iterations without cache
+    curMatrix = hilbert(size)
+    start = Sys.time()
+    for(i in 1:n){
+        tryCatch({
+            m <- solve(curMatrix)
+        }, error = function(err){
+            print(paste0("Error calculating invesre of matrix", err))
+        }, finally = {
+            
+        }
+        )
+    }
+    end = Sys.time()
+    print("withOutCacheCheck")
+    print(end - start)
+}
+
+# solve inverse using local cache
+inverseMatrixWithCache <- function(n, size) {
+    # n - no of iterations
+    # size is the size of the matrix, it will be square matrix
+    # This will try to find time taken for n iterations using local cache
+    
+    curMatrix = hilbert(size)
+    cdata = makeCacheMatrix(curMatrix)
+    start = Sys.time()
+    for(i in 1:n){
+        tryCatch({
+            m <- solve(cdata$get())
+        }, error = function(err){
+            print(paste0("Error calculating invesre of matrix", err))
+        }, finally = {
+            
+        }
+        )
+    }
+    end = Sys.time()
+    print("WithCache")
+    print(end -start)
+}
+
+# To do some perfromance testing 
+runExpr <- function(n){
+    # n - The size of matrix
+    size = as.integer(runif(1, 5, 15))
+    print(paste0("The size =", size))
+    #size = 10
+    inverseMatrixNoCache(n, size)
+    inverseMatrixWithCache(n, size)
 }
   
 
